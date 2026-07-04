539800de52d4d0fbcd048d8ab33031876d1eb97d
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-07-04 08:15:33 +0100

    bump version to 0.1.5 and add functions to fix post and comment trigger events

--- src/redditAPI/postsAndComments.ts
+++ src/redditAPI/postsAndComments.ts
@@ -1,4 +1,5 @@
-import { Comment, Post, RedditAPIClient } from "@devvit/public-api";
+import { PostSubmit, CommentSubmit, PostCreate, CommentCreate, PostUpdate, CommentUpdate } from "@devvit/protos";
+import { Comment, Post, RedditAPIClient, TriggerContext } from "@devvit/public-api";
 import { isCommentId, isLinkId } from "@devvit/public-api/types/tid.js";
 
 export function getPostOrCommentById (reddit: RedditAPIClient, thingId: string): Promise<Post | Comment> {
@@ -9,4 +10,52 @@ export function getPostOrCommentById (reddit: RedditAPIClient, thingId: string):
     } else {
         throw new Error(`Invalid thingId ${thingId}`);
     }
+}
+
+export async function fixPostTriggerEvent<T extends PostSubmit | PostCreate | PostUpdate> (event: T, context: TriggerContext): Promise<T> {
+    const eventToReturn: T = { ...event };
+
+    if (!eventToReturn.post || !eventToReturn.author) {
+        return eventToReturn;
+    }
+
+    if (eventToReturn.author.name !== "[redacted]" && eventToReturn.post.selftext !== "[Removed by Reddit]") {
+        return eventToReturn;
+    }
+
+    const post = await context.reddit.getPostById(eventToReturn.post.id);
+
+    if (post.body !== undefined) {
+        eventToReturn.post.selftext = post.body;
+    }
+
+    eventToReturn.author.name = post.authorName;
+    if (post.authorId) {
+        eventToReturn.author.id = post.authorId;
+    }
+
+    return eventToReturn;
+}
+
+export async function fixCommentTriggerEvent<T extends CommentSubmit | CommentCreate | CommentUpdate> (event: T, context: TriggerContext): Promise<T> {
+    const eventToReturn: T = { ...event };
+
+    if (!eventToReturn.comment || !eventToReturn.author) {
+        return eventToReturn;
+    }
+
+    if (eventToReturn.author.name !== "[redacted]" && eventToReturn.comment.body !== "[Removed by Reddit]") {
+        return eventToReturn;
+    }
+
+    const comment = await context.reddit.getCommentById(eventToReturn.comment.id);
+
+    eventToReturn.comment.body = comment.body;
+
+    eventToReturn.author.name = comment.authorName;
+    if (comment.authorId) {
+        eventToReturn.author.id = comment.authorId;
+    }
+
+    return eventToReturn;
 }
